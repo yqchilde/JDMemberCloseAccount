92f04ca34adff40994fd5e932d3ce6cd3a3c945e
yqchilde <yqchilde@gmail.com>
2022-04-06 18:26:15 +0800

    fix: 修复检测更新

--- utils/version.py
+++ utils/version.py
@@ -1,3 +1,4 @@
+import datetime
 import requests
 
 
@@ -11,14 +12,15 @@ def check_version(logger):
         elif resp.status_code == 200:
             with open('./utils/version', 'r') as f:
                 local_version = f.read()
-                remote_version = resp.text
+                remote_version = resp.text.strip()
             if local_version != remote_version:
                 gitee_api = "https://gitee.com/api/v5/repos/yqchilde/JDMemberCloseAccount/commits?per_page=1"
                 resp = requests.get(gitee_api).json()
                 updated = resp[0]["commit"]["committer"]["date"]
+                updated = datetime.datetime.strptime(updated, "%Y-%m-%dT%H:%M:%S+00:00")
+                updated = updated + datetime.timedelta(hours=8)
                 logger.info(
-                    "检测到项目代码有更新，最近一次更新时间为 [%s]，为了保证项目可用性，希望您尽可能的使用最新版！" %
-                    updated.replace("T", " ").replace("+08:00", "")
+                    "检测到项目代码有更新，最近一次更新时间为 [%s]，为了保证项目可用性，希望您尽可能的使用最新版！" % updated
                 )
 
     except Exception:
